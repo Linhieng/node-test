58bfe457705f0fd7f7de5b2b81be4fbfdc498635
linhieng <790728740@qq.com>
2022-07-04 14:04:27 +0800

    新增几个批处理函数

--- js/班级批量处理相关.js
+++ js/班级批量处理相关.js
@@ -1,19 +1,56 @@
 const fs = require('fs')
-
-const dir = fs.readdirSync('.')
-
+const files = fs.readdirSync('.')
 const { map_1204 } = require('./1204.private')
 
-dir.forEach((v, i) => {
-  const arr = v.split('.')
-  map_1204.some((r) => {
-    const reg = new RegExp(r.name)
-    if (reg.test(arr[0])) {
-      const newName = r.order + r.name + '.' + arr[1]
-      fs.renameSync(v, newName)
-      return true
-    } 
-    return false
+function 批量重命名() {
+  files.forEach((file_name) => {
+    const arr = file_name.split('.')
+    map_1204.some((student) => {
+      const reg = new RegExp(student.name)
+      if (reg.test(arr[0])) {
+        const newName = student.order + student.name + (arr[1] ? '.' + arr[1] : '')
+        fs.renameSync(file_name, newName)
+        return true
+      }
+      return false
+    })
   })
+}
 
-})
+function 查看谁没交作业() {
+  map_1204.forEach(v => {
+    let find = false
+    files.forEach(f => {
+      if (f.match(v.name)) {
+        find = true
+      }
+    })
+    if (!find) console.log(v.name)
+  })
+}
+
+function 查看哪个文件没有名字() {
+  files.forEach(v => {
+    let flag = false
+    map_1204.forEach(n => {
+      if (v.match(n.name)) flag = true
+    })
+    if (!flag) console.log(v)
+  })
+}
+
+function 查看重复名字的文件() {
+  map_1204.forEach(n => {
+    let nums = 0
+    let repeat = []
+    files.forEach(v => {
+      if (v.match(n.name)) {
+        nums++
+        repeat.push(v)
+      }
+    })
+    if (nums > 1) {
+      console.log(repeat)
+    }
+  })
+}
