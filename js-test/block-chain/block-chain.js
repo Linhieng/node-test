0dc3fdc210d728ebf47a9cf8263cc455c810d790
linhieng <790728740@qq.com>
2022-07-09 16:12:32 +0800

    自写消息通知框

--- js-test/block-chain/block-chain.js
+++ js-test/block-chain/block-chain.js
@@ -27,7 +27,7 @@ class Block {
   // 工作量计算
   mineBlock(difficulty) {
     while (
-      this.hash.substring(0, difficulty) !== Array(difficulty + 1).join('0')
+      this.hash.substring(0, difficulty) !== Array(difficulty + 1).join('0') 
     ) {
       this.nonce++
       this.hash = this.calculateHash()
